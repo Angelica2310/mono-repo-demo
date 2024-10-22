1bd33f9359f147bce2fa091402c8b7280eef9719
Angelicale <gianghgle@gmail.com>
2024-10-22 21:43:31 +0100

    API updated

--- client/app.js
+++ client/app.js
@@ -1,7 +1,9 @@
 const h1 = document.querySelector("h1");
 
 async function updateh1() {
-  const response = await fetch("http://localhost:3001/message"); // making a GET request
+  const response = await fetch(
+    "https://mono-repo-demo-exc4.onrender.com/message"
+  ); // making a GET request
   const data = await response.json(); // get the data out of the promise
   h1.textcontent = data;
 }
